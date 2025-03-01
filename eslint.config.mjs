d221e5de98daa640799d45a511ba56b5836baec5
isoftchamp <l.softchamp@gmail.com>
2025-03-01 05:10:26 +0000

    feat: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,37 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["node_modules/*", "**/*.test.ts", "**/*.d.ts"],
+    rules: {
+      // Convert specific warnings to errors
+      "no-warning-comments": [
+        "warn",
+        { terms: ["fixme", "todo"], location: "start" },
+      ],
+
+      // Allow console statements
+      "no-console": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@next/next/no-img-element": "off",
+
+      // Additional rules
+      "import/no-extraneous-dependencies": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "none",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
